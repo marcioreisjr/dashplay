cd85d36657a4f225cf890eb2f8144e1f79f62cc9
Marcio Reis Jr <marcioreisjr@gmail.com>
2024-04-13 11:01:53 -0700

    Step 11 - Added search and pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -10,15 +10,15 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
   const handleSearch = useDebouncedCallback((term) => {
-    console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }, 400);
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
